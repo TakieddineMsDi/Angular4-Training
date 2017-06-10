7379792b6bcb7663ed8108fc9963d8aa8b718ff9
Unknown <takieddine.messaoudi.official@gmail.com>
2017-06-10 21:11:23 +0100

    Input

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloWorldComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
